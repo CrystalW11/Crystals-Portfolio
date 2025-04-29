704bb761639679a5ae52d1d7a8286aff861b5093
CrystalW11 <crystal.warmack@gmail.com>
2025-04-29 17:29:47 -0600

    updated folders

--- src/components/Feedbacks.jsx
+++ src/components/Feedbacks.jsx
@@ -1,13 +1,3 @@
-/** @format */
-
-import React from "react";
-import { motion } from "framer-motion";
-
-import { styles } from "../styles";
-import { SectionWrapper } from "../hoc";
-import { fadeIn, textVariant } from "../utils/motion";
-import { testimonials } from "../constants";
-
 const FeedbackCard = ({
   index,
   testimonial,
@@ -19,7 +9,8 @@ const FeedbackCard = ({
 }) => (
   <motion.div
     variants={fadeIn("", "spring", index * 0.5, 0.75)}
-    className="bg-black-200 p-10 rounded-3xl xs:w-[320px] w-full">
+    className="bg-black-200 p-10 rounded-3xl xs:w-[320px] w-full"
+  >
     <p className="text-white font-black text-[48px]">"</p>
 
     <div className="mt-1">
@@ -27,9 +18,14 @@ const FeedbackCard = ({
 
       <div className="mt-7 flex justify-between items-center gap-1">
         <div className="flex-1 flex flex-col">
-          <p className="text-white font-medium text-[16px]">
+          <a
+            href={source_code_link}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-white font-medium text-[16px] hover:underline"
+          >
             <span className="blue-text-gradient">@</span> {name}
-          </p>
+          </a>
           <p className="mt-1 text-secondary text-[12px]">
             {designation} of {company}
           </p>
@@ -44,24 +40,3 @@ const FeedbackCard = ({
     </div>
   </motion.div>
 );
-
-const Feedbacks = () => {
-  return (
-    <div className={`mt-12 bg-black-100 rounded-[20px]`}>
-      <div
-        className={`bg-tertiary rounded-2xl ${styles.padding} min-h-[300px]`}>
-        <motion.div variants={textVariant()}>
-          <p className={styles.sectionSubText}>What others say</p>
-          <h2 className={styles.sectionHeadText}>Testimonials.</h2>
-        </motion.div>
-      </div>
-      <div className={`-mt-20 pb-14 ${styles.paddingX} flex flex-wrap gap-7`}>
-        {testimonials.map((testimonial, index) => (
-          <FeedbackCard key={testimonial.name} index={index} {...testimonial} />
-        ))}
-      </div>
-    </div>
-  );
-};
-
-export default SectionWrapper(Feedbacks, "");
